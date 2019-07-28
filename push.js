27943249b6644c50a30fd282f7e3b5eeb176eae3
vannvan <shiwanwan@thinkjoy.cn>
2019-07-28 11:48:44 +0800

    2019/07/28 11:48:43

--- push.js
+++ push.js
@@ -3,10 +3,38 @@
 /*
 * @Author: wanwan
 * @Date:   2019-07-15 18:16:58
-* @Last Modified by:   wanwan
-* @Last Modified time: 2019-07-15 18:18:36
+* @Last Modified by:   vannvan
+* @Last Modified time: 2019-07-28 11:47:44
 */
-var name = process.argv[2] || 'auto-commit';
+
+function dateToStr(datetime){
+        var dateTime = new Date(datetime);
+        var year = dateTime.getFullYear();
+        var month = dateTime.getMonth()+1;//js从0开始取
+        var date = dateTime.getDate();
+        var hour = dateTime.getHours();
+        var minutes = dateTime.getMinutes();
+        var second = dateTime.getSeconds();
+ 
+        if(month<10){
+            month = "0" + month;
+        }
+        if(date<10){
+            date = "0" + date;
+        }
+        if(hour <10){
+            hour = "0" + hour;
+        }
+        if(minutes <10){
+            minutes = "0" + minutes;
+        }
+        if(second <10){
+            second = "0" + second ;
+        }
+ 
+        return year+"/"+month+"/"+date+" "+hour+":"+minutes+":"+second;
+    }
+var name = process.argv[2] || dateToStr(new Date());
 var shell = require("shelljs");
 var exec = shell.exec;
 var echo = shell.echo;

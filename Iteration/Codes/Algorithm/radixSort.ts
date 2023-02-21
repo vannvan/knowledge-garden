379bb5fc6844e80ada1f0cfb61ce18655dc51c5b
vannvan <862670198@qq.com>
2023-02-21 22:23:50 +0800

    2023/02/21 22:23:48

--- Iteration/Codes/Algorithm/radixSort.ts
+++ Iteration/Codes/Algorithm/radixSort.ts
@@ -4,12 +4,17 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-02-21 21:59:17
+ * Last Modified: 2023-02-21 22:20:19
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
  */
 
+/**
+ * eg [1,71,231,681,658,892]
+ * @param nums
+ * @returns
+ */
 const radixSort = (nums: number[]) => {
   // 定义一个二维数组，表示10个桶，每个桶就是一个一维数组
   let buckets = new Array(10)
@@ -26,17 +31,24 @@ const radixSort = (nums: number[]) => {
       max = nums[i]
     }
   }
+  // 以最大数的基数为限
+  const maxLen = (max + '').length
 
-  let maxLen = (max + '').length
   // 因为基于基数排序的，每一轮增加一个基数，所以基数在最外层
   for (let i = 0, n = 1; i < maxLen; i++, n = n * 10) {
-    // 第一轮，对每个元素的个位数进行排序
+    // 第一轮，对每个元素的个位数进行排序,依次以十位、百位、千位...进行排序
     for (let j = 0; j < nums.length; j++) {
       // 取出个位数的值
       let digitOfElement = Math.floor(nums[j] / n) % 10 // 0-9
       buckets[digitOfElement][buckeElementCounts[digitOfElement]] = nums[j]
       buckeElementCounts[digitOfElement]++ // 这里处理完是可能有空桶的
     }
+
+    // 第一轮  [1, 101, 672, 302, 782, 444, 989]
+    // 第二轮  [1, 101, 302, 444, 672, 782, 989]
+    // 第三轮  [1, 101, 302, 444, 672, 782, 989]
+
+    // console.log('buckeElementCounts', buckeElementCounts)
 
     // 按照这个桶的顺序，依次对每个桶进行处理,10个桶，一个一个往后找
     let index = 0
@@ -54,6 +66,7 @@ const radixSort = (nums: number[]) => {
         buckeElementCounts[k] = 0
       }
     }
+    // console.log('nums', nums)
   }
   return nums
 }
--- Iteration/Codes/Algorithm/tests/radixSort.test.ts
+++ Iteration/Codes/Algorithm/tests/radixSort.test.ts
@@ -4,7 +4,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-02-21 21:47:11
+ * Last Modified: 2023-02-21 22:19:31
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -15,7 +15,7 @@ import radixSort from '../radixSort'
 describe('基数排序', () => {
   it('radixSort function', () => {
     expect(radixSort([672, 1, 101, 302, 444, 989, 782]))
-    return
+    // return
     expect(radixSort([672, 1, 101, 302, 444, 989, 782])).toEqual([1, 101, 302, 444, 672, 782, 989])
     expect(radixSort([110, 367, 3982, 2189, 121, 232, 4343, 765, 767])).toEqual([
       110, 121, 232, 367, 765, 767, 2189, 3982, 4343,

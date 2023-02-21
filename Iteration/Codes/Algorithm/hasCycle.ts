db38c789e08059d612e3a9f7b08e9f08b2ffdc73
vannvan <862670198@qq.com>
2023-02-21 09:37:51 +0800

    2023/02/21 09:37:46

---
+++ Iteration/Codes/Algorithm/hasCycle.ts
@@ -0,0 +1,24 @@
+/*
+ * Description:
+ * Created: 2023-02-21 09:24:24
+ * Author: van
+ * Email : adoerww@gamil.com
+ * -----
+ * Last Modified: 2023-02-21 09:27:14
+ * Modified By: van
+ * -----
+ * Copyright (c) 2023 https://github.com/vannvan
+ */
+
+const hasCycle = (head) => {
+  let slow = head
+  let fast = head
+  while (fast != null && fast.next != null) {
+    slow = fast.next
+    fast = fast.next.next
+    if (slow == fast) {
+      return true
+    }
+  }
+  return false
+}

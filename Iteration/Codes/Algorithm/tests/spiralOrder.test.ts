0f829a7920c13741924f83f3313d7946454e3e3b
vannvan <862670198@qq.com>
2023-02-27 20:23:11 +0800

    2023/02/27 20:23:06

--- Iteration/Codes/Algorithm/tests/spiralOrder.test.ts
+++ Iteration/Codes/Algorithm/tests/spiralOrder.test.ts
@@ -4,7 +4,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-02-27 20:04:30
+ * Last Modified: 2023-02-27 20:23:00
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -36,8 +36,8 @@ describe('顺时针螺旋顺序输出 测试', () => {
     const target2 = [1, 2, 3, 6, 9, 8, 7, 4, 5]
     const target3 = [1, 2, 3, 4, 8, 12, 11, 10, 9, 5, 6, 7]
 
-    // expect(spiralOrder(source)).toEqual(target)
-    // expect(spiralOrder(source2)).toEqual(target2)
+    expect(spiralOrder(source)).toEqual(target)
+    expect(spiralOrder(source2)).toEqual(target2)
     expect(spiralOrder(source3)).toEqual(target3)
   })
 })

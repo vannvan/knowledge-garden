939facad89fb4dbfcfc78e7c42fb7eae9cbf4e2a
vannvan <862670198@qq.com>
2023-02-28 23:11:07 +0800

    2023/02/28 23:11:02

---
+++ Iteration/Codes/Leetcode/maxProfit.ts
@@ -0,0 +1,39 @@
+/*
+ * Description: 买卖股票的最佳时机
+ * Created: 2023-02-28 22:26:50
+ * Author: van
+ * Email : adoerww@gamil.com
+ * -----
+ * Last Modified: 2023-02-28 23:07:09
+ * Modified By: van
+ * -----
+ * Copyright (c) 2023 https://github.com/vannvan
+ */
+
+const maxProfit = (prices: number[]): number => {
+  // 此方案超出限制
+  // let max = 0
+  // let n: number = prices.length
+  // for (let i = 0; i < n; i++) {
+  //   for (let k = i + 1; k < n; k++) {
+  //     let inc = prices[k] - prices[i]
+  //     if (inc > 0 && inc > max) {
+  //       max = inc
+  //     }
+  //   }
+  // }
+  // return max
+  let minprice = Number.MAX_SAFE_INTEGER
+  let maxprofit = 0
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minprice) {
+      minprice = prices[i] // 假设这一天就是最低值
+    } else if (prices[i] - minprice > maxprofit) {
+      maxprofit = prices[i] - minprice
+    }
+  }
+
+  return maxprofit
+}
+
+export default maxProfit
---
+++ Iteration/Codes/Leetcode/tests/maxProfit.test.ts
@@ -0,0 +1,22 @@
+/*
+ * Description: 买卖股票的最佳时机 测试
+ * Created: 2023-02-28 22:26:50
+ * Author: van
+ * Email : adoerww@gamil.com
+ * -----
+ * Last Modified: 2023-02-28 23:09:13
+ * Modified By: van
+ * -----
+ * Copyright (c) 2023 https://github.com/vannvan
+ */
+
+import maxProfit from '../maxProfit'
+
+describe('买卖股票的最佳时机 测试', () => {
+  it('maxProfit function', () => {
+    maxProfit([7, 1, 5, 3, 6, 4])
+    expect(maxProfit([7, 1, 5, 3, 6, 4])).toEqual(5)
+    expect(maxProfit([7, 1, 5, 3, 6, 4, 1, 8])).toEqual(7)
+    expect(maxProfit([7, 6, 4, 3, 1])).toEqual(0)
+  })
+})

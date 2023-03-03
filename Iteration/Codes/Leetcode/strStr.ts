aa3273a93d1f3bb0b73a71fed7e4a32370d12822
vannvan <862670198@qq.com>
2023-03-03 21:05:53 +0800

    2023/03/03 21:05:48

--- Iteration/Codes/Leetcode/strStr.ts
+++ Iteration/Codes/Leetcode/strStr.ts
@@ -6,7 +6,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-03 19:08:42
+ * Last Modified: 2023-03-03 21:03:18
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -14,9 +14,42 @@
 
 function strStr(haystack: string, needle: string): number {
   // TODO
-  let index = -1
-  //
+  if (needle.length === 0) return 0
 
-  return index
+  const getNext = (str: string): number[] => {
+    let next: number[] = []
+    let j: number = -1
+    next[0] = j
+    for (let i = 1, length = str.length; i < length; i++) {
+      while (j >= 0 && str[i] !== str[j + 1]) {
+        j = next[j]
+      }
+      if (str[i] === str[j + 1]) {
+        j++
+      }
+      next[i] = j
+    }
+    return next
+  }
+
+  let next: number[] = getNext(needle)
+  console.log('next', next)
+
+  let j: number = -1
+  const n = haystack.length
+  for (let i = 0; i < n; i++) {
+    while (j > 0 && haystack[i] != needle[j + 1]) {
+      j = next[j]
+    }
+
+    if (haystack[i] === needle[j + 1]) {
+      if (j === needle.length - 2) {
+        return i - j - 1
+      }
+      j++
+    }
+  }
+
+  return -1
 }
 export default strStr
--- Iteration/Codes/Leetcode/tests/strStr.test.ts
+++ Iteration/Codes/Leetcode/tests/strStr.test.ts
@@ -5,7 +5,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-03 18:59:43
+ * Last Modified: 2023-03-03 21:03:24
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -13,6 +13,8 @@
 import strStr from '../strStr'
 describe('找出字符串中第一个匹配项的下标 测试', () => {
   it('strStr function', () => {
+    strStr('sadbutsad', 'sad')
+    // return
     expect(strStr('sadbutsad', 'sad')).toEqual(0)
     expect(strStr('leetcode', 'leeto')).toEqual(-1)
   })

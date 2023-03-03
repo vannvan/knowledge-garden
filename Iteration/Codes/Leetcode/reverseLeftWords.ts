cbe3e9d875576394bd20555b3d9108aeab4d4561
vannvan <862670198@qq.com>
2023-03-03 18:58:47 +0800

    2023/03/03 18:58:42

--- Iteration/Codes/Leetcode/reverseLeftWords.ts
+++ Iteration/Codes/Leetcode/reverseLeftWords.ts
@@ -6,13 +6,42 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-03 11:10:05
+ * Last Modified: 2023-03-03 18:53:09
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
  */
 
 function reverseLeftWords(s: string, n: number): string {
-  // TODO
+  let strArr = Array.from(s)
+
+  const reverse = (left: number, right: number) => {
+    while (left < right) {
+      let tmp = strArr[right]
+      strArr[right] = strArr[left]
+      strArr[left] = tmp
+      left++
+      right--
+    }
+  }
+
+  for (let i = 0; i <= strArr.length - 1; i++) {
+    // 先翻转 0-n 区间的字符串
+    if (i == n) {
+      reverse(0, n - 1)
+    }
+    // 再翻转 n-结尾的字符串
+    if (i == n) {
+      reverse(n, strArr.length - 1)
+    }
+    // 最后反转整个字符串
+    if (i == strArr.length - 1) {
+      reverse(0, strArr.length - 1)
+    }
+  }
+
+  console.log('strArr', strArr)
+
+  return strArr.join('')
 }
-export default reverseLeftWords;
+export default reverseLeftWords
--- Iteration/Codes/Leetcode/tests/reverseLeftWords.test.ts
+++ Iteration/Codes/Leetcode/tests/reverseLeftWords.test.ts
@@ -5,15 +5,17 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-03 11:10:10
+ * Last Modified: 2023-03-03 18:53:01
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
  */
-import reverseLeftWords from "../reverseLeftWords";
-describe("左旋转字符串 测试", () => {
-  it("reverseLeftWords function", () => {
-    expect(reverseLeftWords("abcdefg", 2));
-    expect(reverseLeftWords("lrloseumgh", 6));
-  });
-});
+import reverseLeftWords from '../reverseLeftWords'
+describe('左旋转字符串 测试', () => {
+  it('reverseLeftWords function', () => {
+    reverseLeftWords('abcdefg', 2)
+    // return
+    expect(reverseLeftWords('abcdefg', 2)).toEqual('cdefgab')
+    expect(reverseLeftWords('lrloseumgh', 6)).toEqual('umghlrlose')
+  })
+})

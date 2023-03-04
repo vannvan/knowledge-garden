de3e9ea72855c7f0ca800dac40fea2cb021db078
vannvan <862670198@qq.com>
2023-03-04 18:44:20 +0800

    2023/03/04 18:43:59

--- Iteration/Codes/Leetcode/repeatedSubstringPattern.ts
+++ Iteration/Codes/Leetcode/repeatedSubstringPattern.ts
@@ -2,17 +2,41 @@
  * Description: 重复的子字符串
  * Url: https://leetcode.cn/problems/repeated-substring-pattern/
  * Tags: 字符串  字符串匹配
- * Created: 2023-03-03 21:06:33
+ * Created: 2023-03-04 15:51:09
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-03 11:10:05
+ * Last Modified: 2023-03-04 16:39:05
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
  */
 
 function repeatedSubstringPattern(s: string): boolean {
-  // TODO
+  // step1 先获取到前缀表
+  // step2 经分析当满足s是有多个重复的子字符串构成的话，其满足 s长度 % (s长度 - next[最后一位]) === 0
+  const getNext = (str: string): number[] => {
+    let next: number[] = []
+    let j: number = 0
+    next[0] = 0
+    for (let i = 1; i < str.length; i++) {
+      while (j > 0 && str[i] !== str[j]) {
+        j = next[j - 1]
+      }
+      if (str[i] === str[j]) {
+        j++
+      }
+      next[i] = j
+    }
+    return next
+  }
+
+  const next = getNext(s)
+
+  const n = s.length
+
+  // console.log('next', getNext(s))
+  if (next[n - 1] != 0 && n % (n - next[n - 1]) === 0) return true
+  return false
 }
-export default repeatedSubstringPattern;
+export default repeatedSubstringPattern
--- Iteration/Codes/Leetcode/tests/repeatedSubstringPattern.test.ts
+++ Iteration/Codes/Leetcode/tests/repeatedSubstringPattern.test.ts
@@ -1,20 +1,21 @@
 /*
  * Description: 重复的子字符串
  * Url: https://leetcode.cn/problems/repeated-substring-pattern/
- * Created: 2023-03-03 21:06:33
+ * Created: 2023-03-04 15:51:09
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-03 11:10:10
+ * Last Modified: 2023-03-04 16:34:54
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
  */
-import repeatedSubstringPattern from "../repeatedSubstringPattern";
-describe("重复的子字符串 测试", () => {
-  it("repeatedSubstringPattern function", () => {
-    expect(repeatedSubstringPattern("abab"));
-    expect(repeatedSubstringPattern("aba"));
-    expect(repeatedSubstringPattern("abcabcabcabc"));
-  });
-});
+import repeatedSubstringPattern from '../repeatedSubstringPattern'
+describe('重复的子字符串 测试', () => {
+  it('repeatedSubstringPattern function', () => {
+    repeatedSubstringPattern('abab')
+    expect(repeatedSubstringPattern('abab')).toEqual(true)
+    expect(repeatedSubstringPattern('aba')).toEqual(false)
+    expect(repeatedSubstringPattern('abcabcabcabc')).toEqual(true)
+  })
+})

993f401245cf7d780200a52b52e5d5375ec5b2f3
vannvan <862670198@qq.com>
2023-03-04 23:42:57 +0800

    2023/03/04 23:42:45

--- Iteration/Codes/Leetcode/tests/topKFrequent.test.ts
+++ Iteration/Codes/Leetcode/tests/topKFrequent.test.ts
@@ -5,7 +5,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-04 22:45:59
+ * Last Modified: 2023-03-04 23:40:19
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -16,5 +16,7 @@ describe('前 K 个高频元素 测试', () => {
     topKFrequent([1, 1, 1, 2, 2, 3], 2)
     expect(topKFrequent([1, 1, 1, 2, 2, 3], 2)).toEqual([1, 2])
     expect(topKFrequent([1], 1)).toEqual([1])
+
+    expect(topKFrequent([4, 1, -1, 2, -1, 2, 3], 2)).toEqual([-1, 2])
   })
 })
--- Iteration/Codes/Leetcode/topKFrequent.ts
+++ Iteration/Codes/Leetcode/topKFrequent.ts
@@ -6,7 +6,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-04 22:46:11
+ * Last Modified: 2023-03-04 23:42:02
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -79,6 +79,136 @@ class Heap {
   }
 }
 
+class MinHeap {
+  heap: any[]
+  constructor() {
+    this.heap = []
+  }
+
+  insert(value: any) {
+    if (value != null) {
+      this.heap.push(value)
+      // 上移，直到父节点小于这个值
+      this.siftUp(this.heap.length - 1)
+      return true
+    }
+    return false
+  }
+
+  siftUp(index) {
+    let parent: any = this.getParentIndex(index)
+    while (index > 0 && this.compare(this.heap[parent], this.heap[index])) {
+      this.swap(this.heap, index, parent)
+      index = parent
+      parent = this.getParentIndex(index)
+    }
+  }
+
+  swap(arr, a, b) {
+    ;[arr[a], arr[b]] = [arr[b], arr[a]]
+    return arr
+  }
+
+  /**
+   * 最小堆 是 a>b  最大堆改为 a<b 即可
+   * 注意 因为head 元素存的是[number,number] 第二项才是频次，所以比较下标为1的元素
+   * @param {*} a
+   * @param {*} b
+   * @returns
+   */
+  compare(a, b) {
+    if (!a || !b) return false
+    return a[1] >= b[1]
+  }
+
+  /**
+   * 移除最小值或最大值
+   * @param {*} value
+   */
+  extract() {
+    if (this.isEmpty()) {
+      return undefined
+    }
+    if (this.size() === 1) {
+      return this.heap.shift()
+    }
+
+    const removedValue = this.heap[0]
+    this.heap[0] = this.heap.pop() // 将 数组末尾的值 赋值给 头部,并删除末尾的值
+    this.siftDown(0)
+    return removedValue
+  }
+  /**
+   * 获取堆顶元素并移除
+   */
+  pop() {
+    let out = this.heap[0]
+
+    // 将最后一个元素放到堆顶
+    this.heap[0] = this.heap.pop()
+
+    // 开始调整
+    let index = 0
+    this.siftDown(index)
+    return out
+  }
+
+  /**
+   * siftDown 做的事情就是 将 父节点 与 左 右子节点比较 ,
+   * @param {*} index
+   */
+  siftDown(index: number) {
+    //向下冒泡
+    let left = this.getLeftIndex(index)
+    let right = this.getRightIndex(index)
+    if (
+      this.compare(this.heap[index], this.heap[left]) &&
+      this.compare(this.heap[index], this.heap[right])
+    ) {
+      if (!this.compare(this.heap[right], this.heap[left])) {
+        this.swap(this.heap, right, index)
+        this.siftDown(right)
+      } else {
+        this.swap(this.heap, left, index)
+        this.siftDown(left)
+      }
+    } else if (this.compare(this.heap[index], this.heap[left])) {
+      this.swap(this.heap, left, index)
+      this.siftDown(left)
+    } else if (this.compare(this.heap[index], this.heap[right])) {
+      this.swap(this.heap, right, index)
+      this.siftDown(right)
+    }
+  }
+
+  isEmpty() {
+    return this.size() === 0
+  }
+
+  size() {
+    return this.heap.length
+  }
+
+  findMinimum() {
+    return this.isEmpty() ? undefined : this.heap[0]
+  }
+
+  getLeftIndex(index) {
+    return 2 * index + 1
+  }
+
+  getRightIndex(index) {
+    return 2 * index + 2
+  }
+
+  getParentIndex(index) {
+    if (index == 0) {
+      return undefined
+    }
+    return Math.floor((index - 1) / 2) // 注意
+  }
+}
+
 function topKFrequent(nums: number[], k: number): number[] {
   let map: Map<number, number> = new Map()
 
@@ -86,22 +216,20 @@ function topKFrequent(nums: number[], k: number): number[] {
     map.set(nums[i], (map.get(nums[i]) || 0) + 1)
   }
 
-  const helperHeap = new Heap((a, b) => a[1] - b[1])
+  const helperHeap = new MinHeap()
 
   for (const entry of map.entries()) {
-    helperHeap.push(entry)
+    helperHeap.insert(entry)
 
     if (helperHeap.size() > k) {
       helperHeap.pop()
     }
   }
-
   const res: number[] = []
 
   for (let i = helperHeap.size() - 1; i >= 0; i--) {
     res[i] = helperHeap.pop()[0]
   }
-  // console.log('helperHeap', helperHeap)
 
   return res
 }

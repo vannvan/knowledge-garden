ce278078d8f40875152cc916b870b3b9e3b13d96
vannvan <862670198@qq.com>
2023-03-07 17:38:51 +0800

    2023/03/07 17:38:46

--- Iteration/Codes/Leetcode/combine.ts
+++ Iteration/Codes/Leetcode/combine.ts
@@ -6,7 +6,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-07 17:36:29
+ * Last Modified: 2023-03-07 17:37:57
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -15,13 +15,13 @@
 function combine(n: number, k: number): number[][] {
   // Think for yourself for 5 minutes...
 
-  let resArr: number[][] = []
+  let res: number[][] = []
 
   const track: number[] = []
 
   const backTracking = (n: number, k: number, startIndex: number): void => {
     if (track.length === k) {
-      resArr.push([...track])
+      res.push([...track])
       return
     }
     // 不剪枝的方式
@@ -32,7 +32,7 @@ function combine(n: number, k: number): number[][] {
     //   track.pop()
     // }
 
-    // 剪枝的方式
+    // 剪枝的操作，当前值和numSum如果已经大于总和了就没必要进入下一次递归了
     for (let i = startIndex; i <= n - k + track.length + 1; i++) {
       track.push(i)
       //通过 startIndex 参数控制树枝的遍历，避免产生重复的子集
@@ -42,6 +42,6 @@ function combine(n: number, k: number): number[][] {
   }
 
   backTracking(n, k, 1)
-  return resArr
+  return res
 }
 export default combine

c741fed7be8205efac1835fb6594c56729cc96bf
vannvan <862670198@qq.com>
2023-03-07 19:49:45 +0800

    2023/03/07 19:49:40

--- Iteration/Codes/Leetcode/tests/letterCombinations.test.ts
+++ Iteration/Codes/Leetcode/tests/letterCombinations.test.ts
@@ -5,7 +5,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-07 19:47:47
+ * Last Modified: 2023-03-07 19:49:38
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -14,7 +14,6 @@ import letterCombinations from '../letterCombinations'
 describe('电话号码的字母组合 测试', () => {
   it('letterCombinations function', () => {
     console.log(letterCombinations('2'))
-    return
     expect(letterCombinations('23')).toEqual(
       expect.arrayContaining(['ad', 'ae', 'af', 'bd', 'be', 'bf', 'cd', 'ce', 'cf'])
     )

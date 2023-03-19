dc100bcfd55457bb3459e790c66c492a0994da4d
vannvan <862670198@qq.com>
2023-03-19 23:08:22 +0800

    2023-03-19 23:08:22

--- Iteration/Codes/Leetcode/largestRectangleArea.ts
+++ Iteration/Codes/Leetcode/largestRectangleArea.ts
@@ -6,11 +6,44 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-19 22:14:07
+ * Last Modified: 2023-03-19 22:28:27
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
  */
+
+/**
+ * 暴力解法
+ * @param heights
+ */
+function largestRectangleArea1(heights: number[]): number {
+  // 对于每一个元素，我们去求它可以向两边延伸，找到能够构成矩形的左右两端点
+  // 能有构成矩形的条件时这个点是>=当前点的
+  let n: number = heights.length
+  if (n == 0) return 0
+  let result: number = 0
+
+  for (let i = 0; i < n; i++) {
+    let left = i
+    let currHeight = heights[i]
+
+    // 找当前元素左边最后 1 个大于等于 heights[i] 的下标
+    while (left > 0 && heights[left - 1] >= currHeight) {
+      left--
+    }
+
+    // 找当前元素右边最后 1 个大于等于 heights[i] 的下标
+    let right = i
+    while (right < n - 1 && heights[right + 1] >= currHeight) {
+      right++
+    }
+
+    let width = right - left + 1
+    result = Math.max(result, width * currHeight)
+  }
+
+  return result
+}
 
 /**
  * 单调递减栈
@@ -41,4 +74,5 @@ function largestRectangleArea(heights: number[]): number {
 
   return result
 }
+
 export default largestRectangleArea

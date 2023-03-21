9bea366ec21fb1c8361dab8d66c70775774ed756
vannvan <862670198@qq.com>
2023-03-21 23:39:45 +0800

    2023-03-21 23:39:45

--- Iteration/Codes/Leetcode/tests/removeDuplicateLetters.test.ts
+++ Iteration/Codes/Leetcode/tests/removeDuplicateLetters.test.ts
@@ -5,7 +5,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-21 23:23:05
+ * Last Modified: 2023-03-21 23:39:27
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -14,7 +14,6 @@ import removeDuplicateLetters from '../removeDuplicateLetters'
 describe('去除重复字母 测试', () => {
   it('removeDuplicateLetters function', () => {
     removeDuplicateLetters('cbacdcbc')
-    return
     expect(removeDuplicateLetters('bcabc')).toEqual('abc')
     expect(removeDuplicateLetters('cbacdcbc')).toEqual('acdb')
   })

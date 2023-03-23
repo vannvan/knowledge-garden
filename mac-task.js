c14f443b9d792e1d4d11bb5da7a08c7b3634a7dd
vannvan <862670198@qq.com>
2023-03-23 10:33:49 +0800

    2023-03-23 10:33:48

--- mac-task.js
+++ mac-task.js
@@ -3,19 +3,19 @@
 // const fs = require("fs");
 
 function dateToStr(datetime) {
-  var dateTime = new Date(datetime);
-  var year = dateTime.getFullYear();
-  var month = dateTime.getMonth() + 1; //js从0开始取
-  var date = dateTime.getDate();
-  var hour = dateTime.getHours();
-  var minutes = dateTime.getMinutes();
-  var second = dateTime.getSeconds();
-  month = month < 10 ? '0' + month : month;
-  date = date < 10 ? '0' + date : date;
-  hour = hour < 10 ? '0' + hour : hour;
-  minutes = minutes < 10 ? '0' + minutes : minutes;
-  second = second < 10 ? '0' + second : second;
-  return year + "/" + month + "/" + date + " " + hour + ":" + minutes + ":" + second;
+  var dateTime = new Date(datetime)
+  var year = dateTime.getFullYear()
+  var month = dateTime.getMonth() + 1 //js从0开始取
+  var date = dateTime.getDate()
+  var hour = dateTime.getHours()
+  var minutes = dateTime.getMinutes()
+  var second = dateTime.getSeconds()
+  month = month < 10 ? '0' + month : month
+  date = date < 10 ? '0' + date : date
+  hour = hour < 10 ? '0' + hour : hour
+  minutes = minutes < 10 ? '0' + minutes : minutes
+  second = second < 10 ? '0' + second : second
+  return year + '/' + month + '/' + date + ' ' + hour + ':' + minutes + ':' + second
 }
 
 // let commitMessage = dateToStr(new Date())
@@ -31,10 +31,8 @@ function dateToStr(datetime) {
 // const {exec} = require('shelljs')
 const exec = require('child_process').exec
 
+// exec('node push')
 
-exec('node push')
-
-
-const now = new Date();const currentTime = `${now.getHours()}:${now.getMinutes()}`;console.log(`推送时间: ${dateToStr(new Date())}`);
-
-
+const now = new Date()
+const currentTime = `${now.getHours()}:${now.getMinutes()}`
+console.log(`推送时间: ${dateToStr(new Date())}`)

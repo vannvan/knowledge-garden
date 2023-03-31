f00b57939ff4e9757a41c3eed109448ca57594de
vannvan <862670198@qq.com>
2023-03-31 23:02:56 +0800

    2023-03-31 23:02:56

--- Iteration/Codes/Leetcode/countNodes.ts
+++ Iteration/Codes/Leetcode/countNodes.ts
@@ -6,7 +6,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-03-31 22:44:16
+ * Last Modified: 2023-03-31 23:02:46
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -26,7 +26,12 @@
  * }
  */
 
-function countNodes(root: TreeNode | null): number {
+/**
+ * 不对
+ * @param root
+ * @returns
+ */
+function countNodes1(root: TreeNode | null): number {
   // Think for yourself for 5 minutes...
   if (!root) return 0
 
@@ -40,4 +45,36 @@ function countNodes(root: TreeNode | null): number {
   dfs(root)
   return count
 }
+
+/**
+ * 易理解版
+ * @param root
+ * @returns
+ */
+function countNodes2(root: TreeNode | null): number {
+  // 递归法计算二叉树节点数
+  // 1. 确定递归函数参数
+  const getNodeSum = function (node) {
+    //2. 确定终止条件
+    if (node === null) {
+      return 0
+    }
+    //3. 确定单层递归逻辑
+    let leftNum = getNodeSum(node.left)
+    let rightNum = getNodeSum(node.right)
+    return leftNum + rightNum + 1
+  }
+  return getNodeSum(root)
+}
+
+/**
+ * 精简版
+ * @param root
+ * @returns
+ */
+function countNodes(root: TreeNode | null): number {
+  if (!root) return 0
+  return countNodes(root.left) + countNodes(root.right) + 1
+}
+
 export default countNodes

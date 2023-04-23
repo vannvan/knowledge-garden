d34dc9389ccde4d1ec0d52fbd663e9e5839fc499
vannvan <862670198@qq.com>
2023-04-24 00:20:16 +0800

    2023-04-24 00:20:16

--- Iteration/Codes/Leetcode/findPeakElement.ts
+++ Iteration/Codes/Leetcode/findPeakElement.ts
@@ -6,14 +6,36 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-04-23 23:50:03
+ * Last Modified: 2023-04-24 00:06:52
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
  */
 
-function findPeakElement(nums: number[]): number {
+function findPeakElement1(nums: number[]): number {
   // Think for yourself for 5 minutes...
-  //
+  let left = 0
+  let right = nums.length - 1
+  while (left < right) {
+    const mid = Math.floor(left + (right - left) / 2)
+    // 当前项大于后一项，那么峰值可以往左边找，
+    if (nums[mid] > nums[mid + 1]) {
+      right = mid
+    } else {
+      left = mid + 1
+    }
+  }
+
+  return left
+}
+
+function findPeakElement(nums: number[]): number {
+  let idx = 0
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > idx) {
+      idx = i
+    }
+  }
+  return idx
 }
 export default findPeakElement
--- Iteration/Codes/Leetcode/tests/findPeakElement.test.ts
+++ Iteration/Codes/Leetcode/tests/findPeakElement.test.ts
@@ -5,7 +5,7 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-04-23 23:50:19
+ * Last Modified: 2023-04-24 00:07:08
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
@@ -14,6 +14,6 @@ import findPeakElement from '../findPeakElement'
 describe('寻找峰值 测试', () => {
   it('findPeakElement function', () => {
     expect(findPeakElement([1, 2, 3, 1])).toEqual(2)
-    expect(findPeakElement([1, 2, 1, 3, 5, 6, 4])).toEqual(1)
+    expect(findPeakElement([1, 2, 1, 3, 5, 6, 4])).toEqual(5)
   })
 })

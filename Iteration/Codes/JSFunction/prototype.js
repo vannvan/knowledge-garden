63e76c8ae18a4750727b603b25c0413b829b6849
vannvan <862670198@qq.com>
2023-05-19 18:30:46 +0800

    2023-05-19 18:30:45

---
+++ Iteration/Codes/JSFunction/prototype.js
@@ -0,0 +1,17 @@
+/*
+ * Description:
+ * Created: 2023-05-16 10:01:19
+ * Author: van
+ * Email : adoerww@gamil.com
+ * -----
+ * Last Modified: 2023-05-16 10:02:18
+ * Modified By: van
+ * -----
+ * Copyright (c) 2023 https://github.com/vannvan
+ */
+function fn() {}
+
+fn.prototype.a = 'aaa'
+
+console.dir(fn.prototype)
+console.log(fn.a)
--- Iteration/Codes/formatCode.ts
+++ Iteration/Codes/formatCode.ts
@@ -4,22 +4,50 @@
  * Author: van
  * Email : adoerww@gamil.com
  * -----
- * Last Modified: 2023-05-03 23:07:25
+ * Last Modified: 2023-05-16 10:05:32
  * Modified By: van
  * -----
  * Copyright (c) 2023 https://github.com/vannvan
  */
-function searchInsert(nums: number[], target: number): number {
-  let left = 0
-  let right = nums.length - 1
-  while (left <= right) {
-    let mid = Math.floor(left + (right - left) / 2)
-    if (nums[mid] === target) return mid
-    else if (nums[mid] < target) {
-      left = mid + 1
-    } else if (nums[mid] > target) {
-      right = mid - 1
+function threeSum(nums: number[]): number[][] {
+  nums.sort((a, b) => a - b)
+  let n = nums.length
+  let left: number = 0
+  let right: number = length - 1
+  const ans: number[][] = []
+
+  for (let i = 0; i < n; i++) {
+    //nums经过排序后，只要nums[i]>0, 此后的nums[i] + nums[left] + nums[right]均大于0,可以提前终止循环。
+    if (nums[i] > 0) continue
+
+    if (i > 0 && nums[i] === nums[i - 1]) {
+      continue
+    }
+
+    left = i + 1
+    right = length - 1
+    while (left < right) {
+      let total: number = nums[i] + nums[left] + nums[right]
+      // 当找到一组匹配的之后，[left,right]区间缩小
+      if (total === 0) {
+        ans.push([nums[i], nums[left], nums[right]])
+        left++
+        right--
+        // 当left左侧和left的值相等时，不会对结果有贡献
+        while (nums[left] === nums[left - 1]) {
+          left++
+        }
+        // 当right右侧的heleft的值相等时，不会对结果有贡献
+        while (nums[right] === nums[right + 1]) {
+          right--
+        }
+      } else if (total > 0) {
+        right--
+      } else {
+        left++
+      }
     }
   }
-  return right + 1
+
+  return ans
 }

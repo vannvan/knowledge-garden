217bef4baede3e6dbd5c281c75c072d082fb8eec
vannvan <862670198@qq.com>
2023-05-23 00:26:30 +0800

    2023-05-23 00:26:25

--- push.js
+++ push.js
@@ -21,43 +21,31 @@ const Log = {
 
 const commitMessage = dayjs().format('YYYY-MM-DD HH:mm:ss')
 
-const cmd = ['git add .', `git commit -am "${commitMessage}"`, 'git push']
+const cmd = ['git add .', `git commit -m "${commitMessage}"`, 'git push']
 
 exec2('git pull -p', 'utf8', (err, stdout, stderr) => {
   if (err) {
     Log.error(err)
   } else {
-    //   const commitMessage = dayjs().format('YYYY-MM-DD HH:mm:ss')
-    //   if (exec2('git add .').code !== 0) {
-    //     Log.error('Error: Git add failed')
-    //     process.exit(1)
-    //   }
-    //   if (exec2(`git commit -am "${commitMessage}"`).code !== 0) {
-    //     Log.error('Error: Git commit failed')
-    //     process.exit(1)
-    //   }
-    //   if (exec2('git push').code !== 0) {
-    //     Log.error('Error: Git push failed')
-    //     process.exit(1)
-    //   }
-    //   Log.success('提交成功！')
-    // }
     Log.success('同步远程成功')
-    const task = cmd.map((item) => {
+    const task = cmd.map((item, index) => {
       return new Promise((resolve, reject) => {
-        exec2(item, 'utf-8', function (err, sto) {
-          if (err) {
-            Log.error(err)
-            reject(err)
-          } else {
-            resolve(`[${item}] success`)
-          }
-        })
+        setTimeout(() => {
+          exec2(item, 'utf-8', function (err, sto) {
+            if (err) {
+              Log.error(err)
+              reject(err)
+            } else {
+              resolve(`[${item}] success`)
+            }
+          })
+        }, index * 500)
       })
     })
     Promise.allSettled(task).then((results) => {
       results.map((item) => {
-        Log.info(item.status)
+        // Log.info(item)
+        console.log(item.value)
       })
     })
   }

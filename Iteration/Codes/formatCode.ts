1348ab1f82aa6952e7b29534f09d0ec4984f7023
vannvan <862670198@qq.com>
2023-05-30 23:31:09 +0800

    2023-05-30 23:31:05

--- Iteration/Codes/formatCode.ts
+++ Iteration/Codes/formatCode.ts
@@ -1,25 +1,15 @@
-function generateParenthesis(n: number): string[] {
-  const ans: string[] = []
-
-  const path: string[] = []
-
-  const dfs = (i: number, open: number) => {
-    if (i == n * 2) {
-      ans.push(path.join(''))
-      return
-    }
-
-    if (open < n) {
-      path[i] = '('
-      dfs(i + 1, open + 1)
-    }
-    if (i - open < open) {
-      path[i] = ')'
-      dfs(i + 1, open)
+function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
+  let count = 0
+  const remove = (node: ListNode, k: number) => {
+    if (node === null) return node
+    node.next = remove(node.next, k)
+    count++
+    if (count === k) {
+      return node.next
+    } else {
+      return node
     }
   }
 
-  dfs(0, 0)
-
-  return ans
+  return remove(head, n)
 }

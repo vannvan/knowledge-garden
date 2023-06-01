9e927aecf4c40f6124f5b53f5dc46b278c8a7c99
vannvan <862670198@qq.com>
2023-06-01 21:54:40 +0800

    2023-06-01 21:54:35

--- Iteration/Codes/formatCode.ts
+++ Iteration/Codes/formatCode.ts
@@ -1,15 +1,35 @@
-function removeNthFromEnd(head: ListNode | null, n: number): ListNode | null {
-  let count = 0
-  const remove = (node: ListNode, k: number) => {
-    if (node === null) return node
-    node.next = remove(node.next, k)
-    count++
-    if (count === k) {
-      return node.next
-    } else {
-      return node
-    }
+function rotateRight(head: ListNode | null, k: number): ListNode | null {
+  if (head === null || head.next === null) return head
+
+  // let len = 0
+  // let cur = head
+  // while (cur) {
+  //   len++
+  //   if (!cur.next) break
+  //   cur = cur.next
+  // }
+  let len: number = 1
+  let cur: ListNode = head
+  while (cur.next) {
+    cur = cur.next
+    len++
   }
 
-  return remove(head, n)
+  // 计算实际要进行旋转的所在点
+  let step: number = len - (k % len)
+
+  // 等于没翻，直接返回
+  if (step === len) return head
+
+  cur.next = head
+
+  while (step) {
+    cur = cur.next
+    step--
+  }
+  // 摘出后半段
+  const rest = cur.next
+  // 掐断前半段原来的关系
+  cur.next = null
+  return rest
 }
